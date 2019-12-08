356a19ecc7e4d3eef146a0bf9c490f6b712c17f0
Monica Howard <mo@Monicas-MacBook-Air.local>
2019-12-08 11:14:12 -0600

    Captures & logs full URL and trimmed path

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('http');
+const url = require('url');
 const PORT = 3000;
 
-
 const server = http.createServer(function(req, res){
-    res.end("Hello, World\n");
-});
+    var parsedURL = url.parse(req.url, true);
+    var path = parsedURL.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+    res.end("Hello, World\n");
+    console.log('Requested | localhost:' + PORT + "/" + trimmedPath + "\n" + "Path |  " + trimmedPath + "\n")
+});
 
 server.listen(PORT, function(){
     console.log("Listening on port " + 3000);
-})
+});
